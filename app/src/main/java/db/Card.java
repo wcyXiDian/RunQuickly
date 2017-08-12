3d701fe02769fbb2f94439edfd4263b31ace4975
wcyXiDian <849417082@qq.com>
2017-08-12 14:56:12 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/db/Card.java
@@ -0,0 +1,50 @@
+package db;
+
+import android.widget.ImageView;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Jock on 2017/8/11.
+ */
+
+public class Card extends DataSupport {
+
+    private int cardFigure;
+    private String cardColor;
+    private int cardValue;
+    private ImageView cardImage;
+
+    public int getCardFigure() {
+        return cardFigure;
+    }
+
+    public void setCardFigure(int cardFigure) {
+        this.cardFigure = cardFigure;
+    }
+
+    public String getCardColor() {
+        return cardColor;
+    }
+
+    public void setCardColor(String cardColor) {
+        this.cardColor = cardColor;
+    }
+
+    public int getCardValue() {
+        return cardValue;
+    }
+
+    public void setCardValue(int cardValue) {
+        this.cardValue = cardValue;
+    }
+
+    public ImageView getCardImage() {
+        return cardImage;
+    }
+
+    public void setCardImage(ImageView cardImage) {
+        this.cardImage = cardImage;
+    }
+
+}
---
+++ app/src/main/java/db/Player.java
@@ -0,0 +1,40 @@
+package db;
+
+import org.litepal.crud.DataSupport;
+
+import java.util.List;
+
+/**
+ * Created by Jock on 2017/8/11.
+ */
+
+public class Player extends DataSupport {
+
+    private int playerId;
+    private List<Card> playerCards;
+    private int playerScore;
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public List<Card> getPlayerCards() {
+        return playerCards;
+    }
+
+    public void setPlayerCards(List<Card> playerCards) {
+        this.playerCards = playerCards;
+    }
+
+    public int getPlayerScore() {
+        return playerScore;
+    }
+
+    public void setPlayerScore(int playerScore) {
+        this.playerScore = playerScore;
+    }
+}
